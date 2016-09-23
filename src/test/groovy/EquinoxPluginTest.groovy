3d50008fa35c4ddf74d9ac559601fe5a7ebd5871
Todor Mazgalov <todor.mazgalov@gmail.com>
2016-09-23 15:57:44 +0300

    Added new test

--- src/test/groovy/EquinoxPluginTest.groovy
+++ src/test/groovy/EquinoxPluginTest.groovy
@@ -47,6 +47,59 @@ plugins {
         printTestOutput result
     }
 
+    def "create-custom-equinox-containers-test"() {
+        given:
+        def testBuildDir = new File(testsBuildDir, name.getMethodName())
+        testBuildDir.deleteDir()
+        testBuildDir.mkdirs()
+        buildFile = new File(testBuildDir, 'build.gradle')
+        buildFile.createNewFile()
+
+        buildFile << """
+plugins {
+    id 'name.mazgalov.equinox'
+}
+
+project.repositories {
+    mavenCentral()
+}
+
+configurations {
+    ds {
+        transitive = false
+    }
+    jline
+}
+
+dependencies {
+    ds group: 'org.apache.felix', name: 'org.apache.felix.scr', version: '2.0.6'
+    ds group: 'org.apache.felix', name: 'org.apache.felix.configadmin', version: '1.8.10'
+    jline group: 'org.apache.servicemix.bundles', name: 'org.apache.servicemix.bundles.jline', version: '0.9.94_1'
+}
+
+task 'createFirstContainer', type: name.mazgalov.equinox.task.CreateEquinoxContainer, {
+    containerName = 'firstEquinox'
+    install 2, configurations.ds
+}
+
+task 'createSecondContainer', type: name.mazgalov.equinox.task.CreateEquinoxContainer, {
+    containerName = 'secondEquinox'
+    install configurations.jline
+}
+"""
+
+        when:
+        println pluginClasspath
+        def result = GradleRunner.create()
+                .withProjectDir(testBuildDir)
+                .withArguments('createFirstContainer', 'createSecondContainer', '-s')
+                .withPluginClasspath(pluginClasspath)
+                .build()
+
+        then:
+        printTestOutput result
+    }
+
     void printTestOutput(def testResult) {
         println "=================== Test: ${name.getMethodName()} ==================="
         println testResult.output
